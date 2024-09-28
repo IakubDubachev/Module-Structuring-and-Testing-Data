30b201137c500dd7f58c52048bcd79020dd969d2
Sam Pennington <56024559+sampennington@users.noreply.github.com>
2024-09-28 23:00:37 +0100

    Add question back in

--- Solouions/Sprint-1/count.js
+++ Solouions/Sprint-1/count.js
@@ -2,9 +2,10 @@ let count = 0;
 
 count = count + 1;
 
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Line 3 is performing an assignment operation. The = operator is assigning a new value to the count variable.
+// Describe what line 3 is doing, in particular focus on what = is doing
+
+// Line 3 is performing an assignment, giving a new value to the count variable declared on line 1.
 // The new value is the result of the expression on the right side: count + 1.
-// This operation is incrementing the count variable by 1, so after this line, count will be 1.
+// So this operation is incrementing the count variable by 1, so after this line, count will now have the value 1.
 
 console.log(`The value of count is now: ${count}`);
