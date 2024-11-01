b1c77bd8b9450b62acd1f5f6b1e904e3b7b41eb4
Iakub Dubachev <gorbone1@gmail.com>
2024-11-01 21:04:53 +0000

    initials task completing

--- Sprint-1/exercises/initials.js
+++ Sprint-1/exercises/initials.js
@@ -1,6 +1,11 @@
 let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
+// Get the first character of each name using charAt and the index 0
+let initials = firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0); // "CKJ"
+
+// Log the initials to the console
+console.log(initials);
 
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
