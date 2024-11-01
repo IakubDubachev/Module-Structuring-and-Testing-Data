f13c79f50e184149eee989d76b6b997fe27d43f1
Iakub Dubachev <gorbone1@gmail.com>
2024-11-01 20:57:30 +0000

    error 4 fixing

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const time24Hour = "20:53";
+const [hours, minutes] = time24Hour.split(":");
+
+const period = hours >= 12 ? "PM" : "AM";
+const hours12 = hours % 12 || 12; // Convert '0' to '12' for midnight/noon
+
+const time12Hour = `${hours12}:${minutes} ${period}`;
+
+console.log(time12Hour);
