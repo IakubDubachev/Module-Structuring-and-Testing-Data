2bca1e2591153bbc34c88f6618b8e669509d1bc1
Iakub Dubachev <gorbone1@gmail.com>
2024-11-01 21:03:36 +0000

    doing decimal task

--- Sprint-1/exercises/decimal.js
+++ Sprint-1/exercises/decimal.js
@@ -1,4 +1,17 @@
 const num = 56.5678;
+// Whole number part
+const wholeNumberPart = Math.floor(num); // 56
+
+// Decimal part
+const decimalPart = num - wholeNumberPart; // 0.5678
+
+// Rounded number
+const roundedNum = Math.round(num); // 57
+
+// Log the variables to the console
+console.log("Whole Number Part:", wholeNumberPart);
+console.log("Decimal Part:", decimalPart);
+console.log("Rounded Number:", roundedNum);
 
 // You should look up Math functions for this exercise https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
