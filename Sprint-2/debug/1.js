c43b8604a949aebada29b6431461bc45e9d52e0f
Iakub Dubachev <gorbone1@gmail.com>
2024-11-10 12:51:25 +0000

    1js in sprint 2

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,6 @@
-// Predict and explain first...
-
 function sum(a, b) {
-  return;
-  a + b;
+  return;  // The function returns undefined, so this line will exit the function before the sum can be computed.
+  a + b;  // This line is never executed due to the return statement above.
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);  // The output will be: "The sum of 10 and 32 is undefined"
