994cdaab2a2a929ccffe5bb9c2ce15108c47ab8f
Iakub Dubachev <gorbone1@gmail.com>
2024-11-10 13:22:25 +0000

    2.js in error section

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,8 @@
-
-// Predict and explain first...
-
-// this function should square any number but instead we're going to get an error
-
-function square(3) {
-    return num * num;
+// Function to square a number
+function square(num) {  // 'num' is the parameter that will hold the input value
+    return num * num;  // Multiply the number by itself to get the square
 }
 
+// Call the function with a number (e.g., 3) and log the result
+console.log(square(3));  // Expected output: 9
 
