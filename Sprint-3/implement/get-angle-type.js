679a0a6f475f76c00d637e67f7fa66da2dc467fb
Iakub Dubachev <gorbone1@gmail.com>
2024-11-16 22:02:17 +0000

    get-angle-type task

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -1,27 +1,24 @@
-// Implement a function getAngleType
+function getAngleType(angle) {
+    if (angle === 90) {
+        return "Right angle";
+    } else if (angle > 0 && angle < 90) {
+        return "Acute angle";
+    } else if (angle > 90 && angle < 180) {
+        return "Obtuse angle";
+    } else if (angle === 180) {
+        return "Straight angle";
+    } else if (angle > 180 && angle < 360) {
+        return "Reflex angle";
+    } else {
+        return "Invalid angle";
+    }
+}
 
-// Acceptance criteria:
+// Example usage:
+console.log(getAngleType(90));  // Output: "Right angle"
+console.log(getAngleType(45));  // Output: "Acute angle"
+console.log(getAngleType(120)); // Output: "Obtuse angle"
+console.log(getAngleType(180)); // Output: "Straight angle"
+console.log(getAngleType(270)); // Output: "Reflex angle"
+console.log(getAngleType(360)); // Output: "Invalid angle"
 
-// Given an angle in degrees,
-// When the function getAngleType is called with this angle,
-// Then it should:
-
-// Identify Right Angles:
-// When the angle is exactly 90 degrees,
-// Then the function should return "Right angle"
-
-// Identify Acute Angles:
-// When the angle is less than 90 degrees,
-// Then the function should return "Acute angle"
-
-// Identify Obtuse Angles:
-// When the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle"
-
-// Identify Straight Angles:
-// When the angle is exactly 180 degrees,
-// Then the function should return "Straight angle"
-
-// Identify Reflex Angles:
-// When the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle"
